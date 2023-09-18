47e7b659e6a13d2420fc5368566abcff7e3d9031
D-Kuma-C <dberwa20@student.aau.dk>
2023-09-18 16:06:43 +0200

    changed some text and deleted some text

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import coop from "./company/Coop.png";
 import danpo from "./company/danpo.png";
 import avk from "./company/avk.png";
@@ -57,10 +53,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -74,8 +74,7 @@ const Works = () => {
           Following projects showcases my skills and
           experience through real-world examples of my work.
           Each projects is briefly described
-          with links to code repositories
-          and live demos in it. It reflects my ability to
+          with links to code repositories. It reflects my ability to
           solve complex problems, work with different technologies,
           and manage projects effectively
         </motion.p>
